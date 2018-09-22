9b20d45fc4b4f68dd086b1021b58f25c9ec41eee
t0shifollow <marcindymek24@gmail.com>
2018-09-22 14:36:58 +0200

    Initial commit -> made classes

---
+++ src/com/t0shi/Main.java
@@ -0,0 +1,8 @@
+package com.t0shi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/t0shi/model/heroes/Knight.java
@@ -0,0 +1,45 @@
+package com.t0shi.model.heroes;
+
+public class Knight {
+
+    private String name;
+
+    private int strenght;
+    private int defence;
+    private int intelligence;
+    private int dextrity;
+    private int agillity;
+    private int speed;
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getDextrity() {
+        return dextrity;
+    }
+
+    public int getAgillity() {
+        return agillity;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
